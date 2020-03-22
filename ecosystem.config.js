7d4624be6ee75e8829779e649ba851f6cbecdf3e
puneet13 <puneet@kontext.in>
2020-03-22 19:18:01 +0530

    dockerised

---
+++ ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+    // First application
+    {
+      name: 'ui',
+      script: 'npm',
+      args: 'start --prefix ui',
+      env: {
+        COMMON_VARIABLE: 'true',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+    {
+      name: 'api',
+      script: 'npm',
+      args: 'start --prefix api',
+      env: {
+        COMMON_VARIABLE: 'true',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
